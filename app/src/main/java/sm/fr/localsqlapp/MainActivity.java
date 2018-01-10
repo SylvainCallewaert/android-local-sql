bad53aef050a84605fc534e4b1840bcaa3738769
formation <sylvain@yahoo.fr>
2018-01-10 11:03:05 +0100

    Conservation de la selection au changement d'orientation du mobil Bug à corriger sur la méthode setSelection de l'objet listView

--- app/src/main/java/sm/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,15 +26,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private List<Map<String,String>> contactList;
     private Integer selectedIndex;
     private Map<String,String> selectedPerson;
+    private final String LIFE_CYCLE = "cycle de vie";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(LIFE_CYCLE,"onCreate");
 
         //référence au widget ListView sur le layout
         contactListView = findViewById(R.id.contactListView);
         contactListInit();
+
+        //Récupération des données persistées dans le Bundle
+        if (savedInstanceState != null){
+            this.selectedIndex = savedInstanceState.getInt("selectedIndex");
+            if (this.selectedIndex != null){
+                this.selectedPerson = this.contactList.get(this.selectedIndex);
+                contactListView.setSelection(this.selectedIndex);
+                Log.i(LIFE_CYCLE,"selection : " + contactListView.getSelectedItem());
+            }
+        }
 
     }
 
@@ -183,5 +196,45 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         this.selectedIndex = position;
         this.selectedPerson = contactList.get(position);
         Toast.makeText(this, "Ligne "+ position + " cliquée", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LIFE_CYCLE, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LIFE_CYCLE,"onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LIFE_CYCLE,"onReseume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LIFE_CYCLE,"onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LIFE_CYCLE, "onStop");
+    }
+
+    /**
+     * Persistance des données avant destruction de l'activité
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("selectedIndex", this.selectedIndex);
+        super.onSaveInstanceState(outState);
     }
 }
